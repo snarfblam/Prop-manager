c0a6da0a92470223504eef449c84bedf88ba015b
Thomas Hudson <snarfblam@gmail.com>
2018-05-07 20:20:55 -0400

    Created some placeholders for file structure

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var db = require('../models');
+var status = require('../util/httpStatus').all;
+
+var router = express.Router();
+
+// add some routes pls
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var Express = require('express');
+////////////// Modules ////////////////////////
+const Express = require('express');
+const path = require('path');
 
-var PORT = process.env.PORT || 3001;
-var app = Express();
-
+////////////// Configuration //////////////////
+const PORT = process.env.PORT || 3001;
+const app = Express();
 app.use(Express.urlencoded());
 app.use(Express.json());
+
+////////////// Routing ////////////////////////
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => {
+    var indexPath = path.join(__dirname, 'client', 'build', 'index.html');
+    res.sendfile(indexPath);
+});
 
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
