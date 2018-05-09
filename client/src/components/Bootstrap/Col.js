e1d8d462e3cecbc5300cab4c22d1eb737371340f
Tom H <snarfblam@gmail.com>
2018-05-08 21:49:28 -0400

    <Col>: Added default class name of 'col' when size is not specified

--- client/src/components/Bootstrap/Col.js
+++ client/src/components/Bootstrap/Col.js
@@ -1,13 +1,23 @@
-import React from 'react';
+/*
+    Implements a bootstrap column element.
+*/
 
 // props.size : string -- space separated list of bootstrap size classes, without the col-prefix
-//                        e.g. '6 md-4 xl-2' -> 'col-6 col-md-4 col-xl-2'
-export default props => {
-    var sizes = (props.size || '').split(' ');
-    sizes = sizes.map(size => 'col-' + size);
-    sizes = sizes.join(' ');
-    
-    if (props.className) sizes += ' ' + props.className;
+//                        e.g. size='6 md-4 xl-2' -> class='col-6 col-md-4 col-xl-2'
 
-    return <div className={sizes}>{props.children}</div>;
+import React from 'react';
+
+export default props => {
+    var sizes = (props.size || '').split(' ').filter(i => i);
+    var classname;
+    
+    if (sizes.length == 0) {
+        classname = 'col'        
+    } else {
+        classname = sizes.map(size => 'col-' + size).join(' ');
+    }
+    
+    if (props.className) classname += ' ' + props.className;
+
+    return <div className={classname}>{props.children}</div>;
 }
