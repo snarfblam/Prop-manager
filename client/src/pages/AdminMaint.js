4f97a9ceb9585a7e6202c8d1ae02c933bcdcadd9
Tom H <snarfblam@gmail.com>
2018-05-10 22:49:45 -0400

    Working <Table> component, Added font-awesome CDN link

--- client/src/pages/AdminMaint.js
+++ client/src/pages/AdminMaint.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavLinkItem, Container } from '../components/Bootstrap';
 import Template from './Template';
 import './page.css'
+import { Table } from '../components/Table';
+import Button from '../components/Bootstrap/Button';
+import Fas from '../components/Fas';
 
 class AdminMaint extends Template {
     constructor(props) {
@@ -22,11 +25,26 @@ class AdminMaint extends Template {
     }
 
     getContent() {
+        var ackButton = <Button><Fas icon='check' />&emsp;Done</Button>
+        var data = {
+            columns: [
+                { name: 'unit', label: 'Unit' },
+                { name: 'date', label: 'Date' },
+                { name: 'message', label: 'Message' },
+                { name: 'status', label: '' },
+            ],
+            items: [
+                { unit: 5, date: '5/10/2018', message: 'Just saying hi!', status: ackButton },
+                { unit: 1, date: '5/8/2018', message: 'heater is on fire, please send help', status: ackButton },
+                { unit: 5, date: '4/29/2018', message: 'roof is leaking, documents ruined, electronics destroyed, seeking compensation. lawsuit pending, please await legal correspondence.', status: ackButton },
+            ]
+        };
+
         return (
             <div>
-                <h1>Maintenance</h1>
-                maint table goes here
-        </div>
+                <h2>Maintenance Requests</h2>
+                <Table data={data} />
+            </div>
         );
     }
 }
