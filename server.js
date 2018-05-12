5887b6f08247b1ddc6631079739f1b56a0cfdcc6
Thomas Hudson <snarfblam@gmail.com>
2018-05-12 13:38:42 -0400

    second try

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 ////////////// Modules ////////////////////////
+require('dotenv').config()
 const Express = require('express');
 const path = require('path');
-// update the config folder with your un and pw. Make sure the DB is created first before server is running
 const db = require('./models');
+const expressSession = require('express-session');
+const SessionStore = require('express-session-sequelize')(expressSession.Store)
+
+// update the config folder with your un and pw. Make sure the DB is created first before server is running
+
+
 ////////////// Configuration //////////////////
 const PORT = process.env.PORT || 3001;
 const app = Express();
 app.use(Express.urlencoded());
 app.use(Express.json());
+
+const sequelizeSessionStore = new SessionStore({
+    db: db.sequelize,
+});
+// app.use(cookieParser());
+app.use(expressSession({
+    secret: 'a whop bop baloobop.',
+    store: sequelizeSessionStore,
+    resave: false,
+    saveUninitialized: false,
+}));
 
 ////////////// Routing ////////////////////////
 app.use(Express.static(path.join(__dirname, 'client', 'build')));
