29d06ec3393ed35891cc5e106c381cae821feaa1
Thomas Hudson <snarfblam@gmail.com>
2018-05-15 20:47:52 -0400

    Implemented <TenantActivate> front-end functionality

--- client/src/api.js
+++ client/src/api.js
@@ -16,9 +16,29 @@ function createNewUser(userData) {
             if (response.status !== 200) throw Error('Could not access server to create user.');
             if (!response.data || !response.data.activationCode) throw Error('Unexpected response from server');
             return { activationCode: response.data.activationCode };
-        }).catch(err => { 
+        }).catch(err => {
+            console.log(err);
             return { error: (err || {}).toString() };
         });
 }
 
-export { createNewUser };
+/**
+ * Assigns an activation code to a user. This must be done prior to the user's first login to allow the user to activate.
+ * Resolves to {status: 'success'} or {error: string}
+ * @param {{activationCode: string}} activationData - Object containing data necessary to activate an account
+ * @returns {Promise<any>}
+ */
+function activateUser(activationData) {
+    return axios
+        .post('api/activateUser/')
+        .then(response => {
+            if (response.status !== 200) throw Error('Could not access server to create user.');
+            if (!response.data || !response.data.status) throw Error('Unexpected response from server');
+            return response.data;
+        }).catch(err => {
+            console.log(err);
+            return { error: (err || {}).toString() };
+        })
+}
+
+export { createNewUser, activateUser };
