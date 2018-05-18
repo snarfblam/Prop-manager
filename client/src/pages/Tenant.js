5c1d8237e9747f1cf7024a67c867ac5835362557
Tom H <snarfblam@gmail.com>
2018-05-18 19:46:39 -0400

    Reworked rent table to use checkboxes and display total for checked invoices

--- client/src/pages/Tenant.js
+++ client/src/pages/Tenant.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+// eslint-disable-next-line
 import { Navbar, NavbarBrand, NavbarNav, NavLinkItem, Container } from '../components/Bootstrap';
 import Template from './Template';
 import './page.css'
@@ -17,6 +18,7 @@ class Tenant extends Template {
         this.submitMaintenanceRequest =this.submitMaintenanceRequest.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.paymentTransform = this.paymentTransform.bind(this);
+        this.paymentCheckChanged = this.paymentCheckChanged.bind(this);
 
         this.rentColumns = [
             { name: 'unitName', label: 'Unit' },
@@ -30,6 +32,7 @@ class Tenant extends Template {
                 columns: this.rentColumns,
                 items: [],
             },
+            checkedPaymentIds: [], // : number[]
             totalDue: 0,
             message: ''
         };
@@ -41,12 +44,15 @@ class Tenant extends Template {
             .getRentDue()
             .then(invoices => {
                 var totalDue = invoices.reduce((acc, item) => acc + item.amount, 0);
+                var checkedPayments = invoices.map(invoice => invoice.id);
+                
                 this.setState({
                     paymentTable: {
                         columns: this.rentColumns,
                         items: invoices,
                     },
                     totalDue: totalDue,
+                    checkedPaymentIds: checkedPayments,
                 });
             });
     }
@@ -78,7 +84,38 @@ class Tenant extends Template {
         .catch(function (error) {
             console.log(error);
         });
-      }
+    }
+    
+    paymentCheckChanged(event) {
+        var checked = event.target.checked || false;
+        var id = parseInt(event.target.name);
+
+        var checkedIds = this.state.checkedPaymentIds.slice();
+
+        if (checked) { // Ensure it's in the list
+            if (!this.state.checkedPaymentIds.includes(id)) {
+                checkedIds.push(id);
+            }
+        } else { // Ensure it's NOT in the list
+            var indexOf = checkedIds.indexOf(id);
+            if (indexOf >= 0) checkedIds.splice(indexOf, 1);
+        }
+
+        var selectedPayments =
+            this.state.paymentTable.items.filter(item => checkedIds.includes(item.id));
+        
+        this.setState({
+            checkedPaymentIds: checkedIds,
+            totalDue: selectedPayments.reduce((total, item) => total + item.amount, 0)
+        });
+    }
+
+    getSelectedPayments() {
+        var allShownPayments = this.state.paymentTable.items.slice();
+        var allCheckedPayments = allShownPayments.filter(payment => this.state.checkedPaymentIds.includes(payment.id));
+
+        return allCheckedPayments;
+    }
 
     /**
      * Converts values from this.state.paymentTable to JSX
@@ -88,7 +125,12 @@ class Tenant extends Template {
      */
     paymentTransform(col, value, item) {
         if (col == 'payButton') {
-            return <Button>Pay</Button>;
+            return <input
+                type='checkbox'
+                checked={this.state.checkedPaymentIds.includes(item.id)}
+                onChange={this.paymentCheckChanged}
+                name={item.id}
+            />;
         } else if (col == 'amount') {
             return this.formatDollars(value);
         } else if (col == 'due') {
@@ -132,21 +174,22 @@ class Tenant extends Template {
         return (
             <div>
                 <h3>Rent Due</h3>
+
+                <Table
+                    data={this.state.paymentTable}
+                    transform={this.paymentTransform}
+                />
                 <p>
                     Total:  <span className='rent-amount'>{this.formatDollars(this.state.totalDue || 0)}</span>
                     <br />
                     <Button
+                        disabled={this.state.totalDue === 0}    
                         onClick={this.payRentWithCreditCard}
                     >
                         Pay Now
                     </Button>
 
                 </p>
-
-                <Table
-                    data={this.state.paymentTable}
-                    transform={this.paymentTransform}
-                />
 
                 <h3>Maintenance Requests</h3>
                 <form>
