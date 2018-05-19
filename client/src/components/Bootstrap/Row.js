51427f5be6db69799fc7d7aa98f37bf0445a7a39
Thomas Hudson <snarfblam@gmail.com>
2018-05-19 13:25:36 -0400

    Added className propogation to <Row>

--- client/src/components/Bootstrap/Row.js
+++ client/src/components/Bootstrap/Row.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export default props => (
-    <div className='row'>
+    <div className={'row' + formatClassname(props.className)}>
         {props.children}    
     </div>
 )
+
+function formatClassname(clsStr) {
+    return clsStr ? (' ' + clsStr) : '';
+}
