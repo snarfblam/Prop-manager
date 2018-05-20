847da61eea41a1042a21b8662ac942d77e4240eb
Tom H <snarfblam@gmail.com>
2018-05-20 12:16:46 -0400

    Modified <Input> to accept prepend and append labels

--- client/src/components/Bootstrap/Input.js
+++ client/src/components/Bootstrap/Input.js
@@ -4,6 +4,8 @@ import Fas from '../Fas';
 // props.name
 //      .value
 //      .label
+//      .innerPrepend
+//      .innerAppend
 //      .password?
 //      .placeholder
 export default props => (
@@ -14,14 +16,26 @@ export default props => (
                 &emsp;<Fas icon='exclamation-circle' /> {props.errorText}
             </span>
         </label>
-        <input
-            className='form-control'
-            type={props.password ? 'password' : 'text'}
-            name={props.name}
-            id={'input-' + props.name}
-            value={props.value}
-            onChange={props.onChange}
-            placeholder={props.placeholder}
-        />
+        <div className='input-group'>
+            {getInputInnerLabel(props.innerLabel, 'prepend')}    
+            <input
+                className='form-control'
+                type={props.password ? 'password' : 'text'}
+                name={props.name}
+                id={'input-' + props.name}
+                value={props.value}
+                onChange={props.onChange}
+                placeholder={props.placeholder}
+                />
+            {getInputInnerLabel(props.innerLabel, 'append')}    
+        </div>    
     </div>    
 );
+
+function getInputInnerLabel(labelText, type) {
+    if (!labelText) return null;
+
+    return (<div className={"input-group-" + type}>
+        <span className="input-group-text">{labelText}</span>
+        </div>);
+}
