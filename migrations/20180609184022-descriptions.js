12f91796854ffea30f94e061025e90272d1ebd03
Tom H <snarfblam@gmail.com>
2018-06-09 15:03:52 -0400

    Added description to 'appSetting' model and created relevant migration

---
+++ migrations/20180609184022-descriptions.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+            'appsettings',
+            'description',
+            {
+                type: Sequelize.DataTypes.STRING,
+                defaultValue: ''
+            }
+        );
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.removeColumn('appsettings', 'description');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
