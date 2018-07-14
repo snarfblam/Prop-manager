2a2abdd999bbea95cc436850be68da659cf7fa5b
Tom H <snarfblam@gmail.com>
2018-07-13 21:28:58 -0400

    disabled sequelize logging

--- models/index.js
+++ models/index.js
@@ -8,9 +8,11 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
+var dontLog = { logging: false };
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], dontLog);
 } else {
+  Object.assign(config, dontLog);
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
